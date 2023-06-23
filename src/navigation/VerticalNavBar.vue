e201539ce4a2cd1685fe97191eb1cb6929bd00ec
Antoni Król <a.krol@ufin.pl>
2023-06-23 18:21:55 +0200

    VNavigation

--- src/navigation/VerticalNavBar.vue
+++ src/navigation/VerticalNavBar.vue
@@ -1,23 +1,30 @@
 <template>
   <VNavigationDrawer
     app
-    :mini-variant="mini"
+    class="drawer"
+    :mini-variant="mini.value"
     :width="drawerWidth"
     :value="drawer"
-    permanent>
+    permanent
+  >
     <VBtn
       variant="text"
-      color="primary"
+      color="white"
+      class="mt-2"
       @click="toggleDrawer"
-      :icon="toggleIcon" />
+      :icon="toggleIcon"
+    />
 
-    <VDivider></VDivider>
+    <VDivider class="divider"></VDivider>
+
     <VList dense>
       <VListItem
         v-for="(item, index) in menuItems"
         :key="index"
-        :to="item.link">
-        <VListItemIcon>
+        :to="item.link"
+        class="white--text"
+      >
+        <VListItemIcon class="white--text">
           <VIcon>{{ item.icon }}</VIcon>
         </VListItemIcon>
         <VListItemContent>
@@ -28,41 +35,44 @@
   </VNavigationDrawer>
 </template>
 
-<script>
-export default {
-  name: "VerticalNavigation",
-  data() {
-    return {
-      drawer: true,
-      mini: false,
-      menuItems: [
-        { title: "Start", icon: "mdi-home", link: "/" },
-        { title: "Bryły 3D", icon: "mdi-cube-outline", link: "/hand" },
-        {
-          title: "Tablica",
-          icon: "mdi-human-male-board",
-          link: "/white-board",
-        },
-        {
-          title: "Kształty 2D",
-          icon: "mdi-triangle",
-          link: "/2d-shapes",
-        },
-      ],
-    };
-  },
-  computed: {
-    toggleIcon() {
-      return this.mini ? "mdi-chevron-right" : "mdi-chevron-left";
-    },
-    drawerWidth() {
-      return this.mini ? 120 : 56;
-    },
-  },
-  methods: {
-    toggleDrawer() {
-      this.mini = !this.mini;
-    },
-  },
-};
+<script setup>
+import { ref, computed } from 'vue'
+
+// Data
+const drawer = ref(true)
+const mini = ref(false)
+const menuItems = ref([
+  { title: "Start", icon: "mdi-home", link: "/" },
+  { title: "Bryły 3D", icon: "mdi-cube-outline", link: "/hand" },
+  { title: "Tablica", icon: "mdi-human-male-board", link: "/white-board" },
+  { title: "Kształty 2D", icon: "mdi-triangle", link: "/2d-shapes" },
+])
+
+// Computed
+const toggleIcon = computed(() => (mini.value ? "mdi-chevron-right" : "mdi-chevron-left"))
+const drawerWidth = computed(() => (mini.value ? 120 : 56))
+
+// Methods
+function toggleDrawer() {
+  mini.value = !mini.value
+}
 </script>
+
+<style scoped>
+.drawer {
+  background: linear-gradient(to bottom, #673ab7, #512da8);
+  box-shadow: 10px 0px 20px rgba(0, 0, 0, 0.15);
+}
+
+.divider {
+  background: white;
+}
+
+.v-list-item {
+  transition: background 0.3s ease;
+}
+
+.v-list-item:hover {
+  background: rgba(255, 255, 255, 0.1);
+}
+</style>
